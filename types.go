715e1137d38e547d2725845ed6d52faefe062e3e
Ta-Ching Chen <contact@tachingchen.com>
2018-03-14 02:27:04 +0800

    Add support for updating grace period through CLI

--- types.go
+++ types.go
@@ -244,7 +244,7 @@ type (
 
 		// The grace time for pod to perform connection draining before termination. The unit is in seconds.
 		// Optional, defaults to 360 seconds
-		TerminationGracePeriod int
+		TerminationGracePeriod int64
 	}
 
 	AllowedFunctionsPerContainer string
